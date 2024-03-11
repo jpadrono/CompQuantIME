15aae06edc2f1f48d713d4b7be3f9452fe6cb84f
jpadrono <joaopsmbraga@gmail.com>
2024-03-11 09:54:40 -0300

    Update exercicio6.py

--- Exercicios_Aula_2/exercicio6.py
+++ Exercicios_Aula_2/exercicio6.py
@@ -3,9 +3,11 @@ str2 = input("Digite o segundo complexo no formato a+bj: ")
 
 c1 = complex(str1)
 c2 = complex(str2)
+tol = 10e-5
 
+# uso da tolerância devido ao erro associado a operaçoes com pontos flutuantes
 print("teste a:")
-print("|c1|*|c2| = |c1*c2|", (abs(c1)*abs(c2) == abs(c1*c2)))
+print("|c1|*|c2| = |c1*c2|", (abs(c1)*abs(c2) - abs(c1*c2) < tol))
 
 print("teste b:")
 print("|c1+c2| <= |c1|+|c2|", (abs(c1+c2) <= abs(c1)+abs(c2)))
